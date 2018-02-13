6df4c50a867d62bc4508ce0a2f89a7a0ff10175e
Pravdaray <pravda.ray@gmail.com>
2018-02-13 14:07:50 +0100

    Assignment 1 completed

--- inf5860_oblig1/inf5860/classifiers/softmax.py
+++ inf5860_oblig1/inf5860/classifiers/softmax.py
@@ -31,7 +31,29 @@ def softmax_loss_naive(W, X, y, reg):
   #############################################################################
   #loss=[]
   #dw = []
-  pass
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  for i in xrange(num_train):
+   #here we define loss
+      scores = np.dot(X[i], W)
+      scores -= np.max(scores)
+      #softmax = np.exp(scores)/np.sum(np.exp(scores)
+      scores_sum = np.sum(np.exp(scores))
+      exp_score = np.exp(scores[y[i]])
+      # the loss update
+      loss += -np.log(exp_score / scores_sum)
+      # the gradient update
+      dW[:, y[i]] -= X[i]
+      for j in xrange(num_classes):
+          dW[:, j] += X[i] * np.exp(scores[j]) /scores_sum
+
+  loss /= num_train  
+  #loss += 0.5 * reg * np.sum(W*W)
+  dW /= num_train
+  loss += reg * np.sum(W * W)
+  dW += 2 * reg*W 
+        
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -57,6 +79,29 @@ def softmax_loss_vectorized(W, X, y, reg):
   #############################################################################
   #loss = []
   #dW = []
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+
+  scores = np.dot(X, W)
+  scores -= scores.max()
+  exp_scores = np.exp(scores)
+  score_sum = np.sum(exp_scores, axis=1)
+  prob = exp_scores[range(num_train), y]
+  loss = prob / score_sum
+  loss = -np.sum(-np.log(loss))/num_train + reg * np.sum(W *W)
+
+
+ 
+   #Gradient
+
+  G = np.divide(exp_scores, score_sum.reshape(num_train, 1))
+  G[range(num_train), y] = - (score_sum - prob) / score_sum
+  #G = np.equal(y[np.newaxis, :], np.arange(C)[:, np.newaxis]
+  dW = X.T.dot(G)
+  #dW += np.dot(exp_score.T / norm, X).T
+
+  dW /=num_train
+  dW += 2 *reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
